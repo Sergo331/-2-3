2cee86345b2747fa0722c4ccfc367c132eca492d
Sergo331 <proha331@gmail.com>
2023-04-26 19:06:31 +0300

    задача 2-3
    задача 2-3

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace задача_2_3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Используем цикл for тк у нас есть определенные границы цикла
+            int lowerBound = 5;
+            int upperBound = 100;
+            int circleStep =7;
+
+            for(int i = lowerBound; i < upperBound; i += circleStep ) 
+            {
+                Console.Write(i + " ");
+            }
+        }
+    }
+}
